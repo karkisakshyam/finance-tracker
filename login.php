6df030ef3c2dcdb1d8b25cba364768dc680ba52c
dipen-0206 <thapadipen88@gmail.com>
2025-06-23 13:30:44 +0545

    dash

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
 session_start();
 include 'database/db-conn.php';
 
@@ -6,33 +7,35 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $email = $conn->real_escape_string($_POST['email']);
     $password = $_POST['password'];
-}
 
-$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+    $sql = "SELECT id, username, password FROM users WHERE email = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('s', $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-$sql =" SELECT ID, username, password FROM users WHERE email= ?";
-$stmt= $conn->prepare($sql);
-$stmt ->bind_param('s', $email);
-$stmt -> execute ();
-$result = $stmt -> get_result();
-
-if ( $result ->num_rows == 1) {
-    $user = $result ->fetch_assoc();
-     if (password_verify($password, $user['password'])) {
+    if ($result && $result->num_rows === 1) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])) {
             $_SESSION['user_id'] = $user['id'];
             $_SESSION['username'] = $user['username'];
-            header("Location: dashboard.php");
-            exit();
 
-
+             session_start();
+    $_SESSION['is_loggedin'] = true;
+    header("Location: dashboard.php");
+} else {
+    header("Location: login.php?error=email or password incorrect");
 }
+    //         header("Location: dashboard.php");
+    //         exit();
+    //     } else {
+    //         $error = "Invalid email or password";
+        }
+    // } else {
+    //     $error = "Invalid email or password";
+    // }
 }
-
-
-
-
-
-
+ob_end_flush();
 ?>
 
 <!DOCTYPE html>
